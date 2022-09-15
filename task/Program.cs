b818ecf30c1ed34e0d5ecf6a3705782a2639539c
Elena <lelya2402@rambler.ru>
2022-09-15 14:35:58 +0300

    решение задачи

--- task/Program.cs
+++ task/Program.cs
@@ -11,3 +11,51 @@
 
 */
 
+string [] StringArray(string[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=Console.ReadLine();
+
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i=0; i<array.Length;i++)
+    {       
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] NewStringArray(string[] array)
+{
+    string[] result=new string[array.Length];
+    int k=0;
+    for (int i=0; i<array.Length;i++)
+    {
+        if (array[i].Length<=3)
+        {
+            result[k]=(array[i]);
+            k++;
+        }        
+    }
+return result;
+}
+
+Console.Write("Сколько слов будет вводиться? - ");
+int leng=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите {leng} слов(а):");
+string [] Array=new string[leng];
+StringArray(Array);
+Console.WriteLine("Получился такой масив:");
+PrintArray(Array);
+
+Console.WriteLine();
+
+string[] newArray= NewStringArray(Array);
+Console.WriteLine("Новый масcив, в котором длина строк <=3 : ");
+PrintArray(newArray);
